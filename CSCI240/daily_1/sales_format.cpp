3aa4acc2a1387eb94b542efc44f2f9de9ddfeb91
BoltR6 <johnwallacewhite@gmail.com>
2024-02-01 08:11:50 -0600

    Changed to doubles, ty Fluke

--- CSCI240/daily_1/sales_format.cpp
+++ CSCI240/daily_1/sales_format.cpp
@@ -20,10 +20,10 @@ using namespace std;
  * @brief A checked input that prevents an invalid conversion.
  *
  * @param string String which will prompt prior to taking input
- * @return Returns a now-parsed typename T
+ * @return Returns a now-parsed value
  */
-template <typename T> T checked_input ( string msg ) {
-    T ret;
+double checked_input_double ( string msg ) {
+    double ret;
 
     // Continually ask for input until manual loop termination
     while ( true ) {
@@ -35,7 +35,31 @@ template <typename T> T checked_input ( string msg ) {
             cin >> input;
 
             // Attempt to parse to a float
-            ret = stof(input);
+            ret = stod(input);
+
+            // Only reachable if the input parses
+            break;
+        } catch ( const std::invalid_argument& e ) {
+            cout << "\tInvalid input!" << endl;
+        }
+    }
+
+    return ret;
+}
+int checked_input_int ( string msg ) {
+    int ret;
+
+    // Continually ask for input until manual loop termination
+    while ( true ) {
+        try {
+            string input;
+
+            // Output template message and direct to input string
+            cout << msg;
+            cin >> input;
+
+            // Attempt to parse to a float
+            ret = stoi(input);
 
             // Only reachable if the input parses
             break;
@@ -48,10 +72,10 @@ template <typename T> T checked_input ( string msg ) {
 }
 
 int main(){
-    float item_price = checked_input<float>("What is the price for a single item? ");
-    int number_items = checked_input<int>("How many items were purchased? ");
+    double item_price = checked_input_double("What is the price for a single item? ");
+    int number_items = checked_input_int("How many items were purchased? ");
 
-    float total_cost = item_price * number_items;
+    double total_cost = item_price * number_items;
 
     cout << endl;
     cout << right << setw(5) << number_items << " items at $" << fixed << setprecision(2) << item_price << " each will cost $" << setprecision(2) << total_cost << endl;
