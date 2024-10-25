eed11614b85bc13f06e473c83692cfb5f170c2f3
hiibolt <my_git_username@gmail.com>
2024-10-24 21:26:13 -0500

    chore: Added signature

--- CSCI241/assignments/inpost/inpost.cpp
+++ CSCI241/assignments/inpost/inpost.cpp
@@ -1,9 +1,31 @@
+/***************************************************************
+CSCI 241         John White     Spring 2024
+
+Programmer: z19942444
+
+Section: 241-0002
+
+Date Due: October 27th
+
+Purpose: To convert an infix expression to postfix
+***************************************************************/
+
+/*
+
+    /ᐠ - ˕ -マ
+
+    @hiibolt on GitHub 
+
+*/
+
+
 #include <string>
 #include <iostream>
 #include <cctype>
 
 #include "inpost.h"
 #include "mystack.h"
+
 
 std::string convert ( const std::string& _infix ) {
 	// Define our operator stack and postfix buffer
--- CSCI241/assignments/inpost/inpost.h
+++ CSCI241/assignments/inpost/inpost.h
@@ -1,3 +1,24 @@
+/***************************************************************
+CSCI 241         John White     Spring 2024
+
+Programmer: z19942444
+
+Section: 241-0002
+
+Date Due: October 27th
+
+Purpose: To convert an infix expression to postfix
+***************************************************************/
+
+/*
+
+    /ᐠ - ˕ -マ
+
+    @hiibolt on GitHub 
+
+*/
+
+
 #ifndef INPOST_H
 #define INPOST_H
 
