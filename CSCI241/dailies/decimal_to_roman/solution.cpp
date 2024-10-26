8699ff71da2c2a3933860537ca48cb2ad9b29f3b
hiibolt <my_git_username@gmail.com>
2024-10-26 18:26:36 -0500

    feat: Completed daily `decimal_to_roman`

--- CSCI241/dailies/decimal_to_roman/solution.cpp
+++ CSCI241/dailies/decimal_to_roman/solution.cpp
@@ -5,36 +5,51 @@
 int value ( int i ) {
 	return pow ( 10, i / 2 ) * ( i % 2 == 0 ? 1 : 5 );
 }
-std::string decimal_to_roman ( int dec ) {
+std::string decimal_to_roman ( int num ) {
 	std::string ret = "";
 	
-	char letters[7] = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+	while ( num > 0 ) {
+            if ( num >= 1000 ) {
+                num -= 1000;
+                ret += 'M';
+            } else if ( num >= 900 ) {
+                num -= 900;
+                ret += "CM";
+            } else if ( num >= 500 ) {
+                num -= 500;
+                ret += 'D';
+            } else if ( num >= 400 ) {
+                num -= 400;
+                ret += "CD";
+            } else if ( num >= 100 ) {
+                num -= 100;
+                ret += 'C';
+            } else if ( num >= 90 ) {
+                num -= 90;
+                ret += "XC";
+            } else if ( num >= 50 ) {
+                num -= 50;
+                ret += 'L';
+            } else if ( num >= 40 ) {
+                num -= 40;
+                ret += "XL";
+            } else if ( num >= 10 ) {
+                num -= 10;
+                ret += 'X';
+            } else if ( num >= 9 ) {
+                num -= 9;
+                ret += "IX";
+            } else if ( num >= 5 ) {
+                num -= 5;
+                ret += 'V';
+            } else if ( num >= 4 ) {
+                num -= 4;
+                ret += "IV";
+            } else {
+                num -= 1;
+                ret += 'I';
+            }
+        }
 
-	for ( int i = 0; i < 7; i++ ) {
-		//std::cout << letters[i] << ": " << value(i) << std::endl;
-	}
-
-	while ( dec > 0 ) {
-		std::cout << dec << std::endl;
-
-		for ( int i = 6; i > 0; i-- ) {
-			std::cout << "Checking " << letters[i] << "(" << value(i) << ")" << std::endl;
-			if ( dec >= value(i) ) {
-				ret += letters[i];
-				dec -= value(i);
-				break;
-			} else if ( dec >= value(i) - value(i - 1) ) {
-				ret += letters[i];
-				ret += letters[i - 1];
-				dec -= value(i) - value(i - 1);
-				break;
-			}
-		}
-		
-		while ( dec < 4 && dec > 0 ) {
-			ret += 'I';
-			dec -= 1;
-		}
-	}
 	return ret;
 };
